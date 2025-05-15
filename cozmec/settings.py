2c51c3ea3308b804331881301abc08e981d2d65c
jincy <jincy.tinos@gmail.com>
2025-05-15 19:21:11 +0530

    allowed host

--- cozmec/settings.py
+++ cozmec/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@aspy1ms@80c=x+t_e-p2grj-(1)7$aqfq#^apv0hyb3au^1of
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['cozmek.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
